c02f34b7f3cf6b1f1eb6fa814c5e94073ca900bf
Guilherme <guilherme.ferreira28@outlook.com>
2019-09-23 16:27:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,13 +18,17 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  
+  var language = req.headers['accept-language'];
+  var software = req.headers['user-agent'];
+  
+  res.json({'ipaddress': ip, 'language': language, 'software': software});
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
